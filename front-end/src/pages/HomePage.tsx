5a28c59e7e7c89c7acf21af5515148acc88e219e
William Morales <william.rolando88@hotmail.com>
2023-04-20 21:45:09 -0500

    Update page design

--- front-end/src/pages/HomePage.tsx
+++ front-end/src/pages/HomePage.tsx
@@ -7,14 +7,18 @@ const HomePage: FC = () => {
       alignItems="center"
       height="100%"
       justifyContent="center"
-      sx={{ backgroundColor: (theme) => theme.palette.background.paper }}
+      bgcolor="whitesmoke"
     >
       <Paper elevation={2} sx={{ width: 400 }}>
         <Stack gap={2} alignItems="center" padding={4}>
-          <Typography>Upload your image</Typography>
-          <Typography>File should be JPEG, PNG...</Typography>
+          <Typography variant="h5">Upload your image</Typography>
+          <Typography variant="caption" color="GrayText">
+            File should be JPEG, PNG...
+          </Typography>
           Here comes the dropzone
-          <Typography>Or</Typography>
+          <Typography variant="caption" color="GrayText">
+            Or
+          </Typography>
           <Button variant="contained">Choose a file</Button>
         </Stack>
       </Paper>
